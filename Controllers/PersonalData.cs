992a98163984ae741c442ba0f533bbf5402f0369
Wayne Black <wblack@smchcn.net>
2014-07-01 20:59:40 -0500

    Including personal data and account controller

---
+++ Controllers/PersonalData.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace WorkApplications.Controllers
+{
+    public class PersonalData : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
