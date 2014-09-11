194f6f09f868b9e0396cee3b8d76ec9e45126215
Wayne Black <wblack@smchcn.net>
2014-09-11 16:53:44 -0500

    Moved JQuery scripts

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -9,14 +9,14 @@ namespace WorkApplications
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/JQuery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/JQuery/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/JQuery/jquery.unobtrusive*",
+                        "~/Scripts/JQuery/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
